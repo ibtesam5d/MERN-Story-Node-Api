18751b45c4f470d475b3f45ed40dd076e0679554
ibtesam5d <ibtesam.hossain@gmail.com>
2023-03-25 12:33:06 -0600

    cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,8 +1,6 @@
 const whiteList = [
   "https://mdibtesamhossain.com",
-  "http://127.0.0.1:5173",
-  "http://127.0.0.1:3000",
-  "http://localhost:5173",
+  "https://mdibtesamhossain.com/story",
 ];
 const corsOptions = {
   origin: (origin, callback) => {
