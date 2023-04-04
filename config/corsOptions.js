8079da058e6bef59c97492c203c49b9ca46767c7
Md Ibtesam Hossain <96791969+ibtesam5d@users.noreply.github.com>
2023-04-04 15:42:26 -0600

    Update corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,8 @@
 const whiteList = [
   "https://mdibtesamhossain.com",
-  "http://localhost:5000","http://localhost:5173","http://127.0.0.1:5173",
+  "http://localhost:5000",
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
   "https://story-fe.onrender.com",
 ];
 const corsOptions = {
